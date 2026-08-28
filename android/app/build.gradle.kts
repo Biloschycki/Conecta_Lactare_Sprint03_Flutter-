297f695f2a11b19d676f8295b7819d9999bcf686
Julia Dutra <juliamendespes@gmail.com>
2026-08-28 19:38:04 -0300

    fix: corrige applicationId Android (com.syncare.conectalactare), adiciona go_router ao pubspec e limpa arquivos de configuração

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,30 +1,27 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.conecta_lactare"
+    namespace = "com.syncare.conectalactare"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.conecta_lactare"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Mesmo applicationId do projeto Android original (Kotlin/Compose).
+        applicationId = "com.syncare.conectalactare"
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -32,8 +29,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Assinado com as chaves de debug por padrão, para permitir `flutter run --release`.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
--- android/app/src/main/kotlin/com/example/conecta_lactare/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.conecta_lactare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
